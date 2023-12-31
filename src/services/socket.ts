a00fc9b4aac678d4f143a64da00457f9ca99d5b9
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-12-31 20:29:08 +0200

    update socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,5 @@
 import io, { Socket, connect } from 'socket.io-client';
 import { socketURL } from '../config/api';
-import { IUpdateTable } from '../types/table.type';
 
 export class SocketService {
   static socket: null | Socket = null;
@@ -16,28 +15,4 @@ export class SocketService {
       console.log('Disconnection');
     });
   }
-
-  //
-  //
-  //
-
-  // updateTable(data: IUpdateTable[]) {
-  //   this.socket.emit('updateTable', data);
-  // }
-
-  // newTableInfo() {
-  //   return new Promise<any>((resolve, reject) => {
-  //     this.socket.on('newTableInfo', (data) => {
-  //       resolve(data);
-  //     });
-  //   });
-  // }
-
-  // tableInfo = () => {
-  //   return new Promise<any>((resolve, reject) => {
-  //     this.socket.on('tableInfo', (data) => {
-  //       resolve(data);
-  //     });
-  //   });
-  // };
 }
